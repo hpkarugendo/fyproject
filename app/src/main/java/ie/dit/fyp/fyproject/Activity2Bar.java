c5d4c0b29fa323571de05c7615151c34f3aa3630
HPKarugendo <c12716891@mydit.ie>
2016-02-22 17:37:36 +0000

    Added Kitchen and Bar Activities, Views and Layouts

---
+++ app/src/main/java/ie/dit/fyp/fyproject/Activity2Bar.java
@@ -0,0 +1,13 @@
+package ie.dit.fyp.fyproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2Bar extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_bar);
+    }
+}
---
+++ app/src/main/java/ie/dit/fyp/fyproject/Activity2KitchenItem.java
@@ -0,0 +1,13 @@
+package ie.dit.fyp.fyproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2KitchenItem extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_kitchen_item);
+    }
+}
